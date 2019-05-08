1cc977af2e36f1d7267ab4f2cb8614092fc7fab3
Gian Libunao <gianlibunao@gmail.com>
2019-05-08 17:49:11 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
+require "pry"
+
 def alphabetize(arr)
   # code here
+  ea = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  eaArray = ea.split("")
+  counter = 0
+  arr.each{|a| arr.each{|b| (a[counter]<=>b[counter])==0 ? counter+=1 : counter}
+  counter-=1
+  }
+  #binding.pry
+  arr.sort_by{|x| eaArray.index(x[counter])}
+  
 end
